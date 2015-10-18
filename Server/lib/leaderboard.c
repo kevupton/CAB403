56a6450e386e2903da27e96f3e4642b150c983a9
Kevin Upton <k_upton@live.com>
2015-10-18 14:12:05 +1000

    begun commenting

--- Server/lib/leaderboard.c
+++ Server/lib/leaderboard.c
@@ -6,7 +6,17 @@
 #include <stdio.h>
 #include "leaderboard.h"
 
+
+/**
+ * Initialises the Leaderboard class
+ *
+ * @param start_from the nb to start from
+ * @param count the nb of scores to take
+ *
+ * @return Leaderboad* the instance of the Leaderboard
+ */
 Leaderboard *newLeaderboard(int start_from, int count) {
+    //allocate the memory
     Leaderboard *l = malloc(sizeof(Leaderboard));
     l->names = malloc(count * sizeof(char*));
     l->wins = malloc(count * sizeof(int));
@@ -15,9 +25,10 @@ Leaderboard *newLeaderboard(int start_from, int count) {
 
     int i = 0;
     User *u;
+    //assign the users to the memory.
     for (i = 0; i < count && (i + start_from) < control->users->count; i++) {
         u = control->users->items[i + start_from];
-        if (u->played > 0) {
+        if (u->played > 0) { //only if they have played a game
             l->names[l->count] = u->username;
             l->wins[l->count] = u->won;
             l->played[l->count] = u->played;
@@ -28,14 +39,25 @@ Leaderboard *newLeaderboard(int start_from, int count) {
     return l;
 }
 
+/**
+ * Gets the intended position of the selected user with the newly updated score.
+ *
+ * @param high_pos the location of the highest ranking position i.e. 0
+ * @param low_pos the location of the lowest ranking position i.e. count - 1
+ * @param user the user to use in the search
+ *
+ * @return int the new allocated position
+ */
 int _get_score_pos(const int high_pos, const int low_pos, const User *user) {
     int half_pos = (low_pos - high_pos)/2 + high_pos;
 
+    //get the users of each position
     User    *low  = control->users->items[low_pos],
             *high = control->users->items[high_pos],
             *half = control->users->items[half_pos];
 
     if (high_pos == low_pos) return high_pos;
+    //check if the users are equal in any way, if so then move the pos
     if (high == user) {
         high = control->users->items[high_pos + 1];
     }
@@ -46,6 +68,7 @@ int _get_score_pos(const int high_pos, const int low_pos, const User *user) {
         half = control->users->items[half_pos - 1];
     }
 
+    //work out where it sits using a binary search
     if (user->won > high->won) {
         return high_pos;
     } else if (user->won == high->won) {
@@ -66,6 +89,15 @@ int _get_score_pos(const int high_pos, const int low_pos, const User *user) {
     }
 }
 
+/**
+ * Gets the sub position if the positions end up being equal.
+ * Matches on the next level, which is the number of games played.
+ *
+ * @param user the user to search for.
+ * @param pos the position of the search.
+ *
+ * @return the new int position.
+ */
 int _get_sub_pos(const User *user, int pos) {
     void **items = control->users->items;
     int x;
@@ -73,6 +105,7 @@ int _get_sub_pos(const User *user, int pos) {
     x = pos;
     User *userx = (User *) items[x];
 
+    //check the list going up. until it no longer fits.
     while (x >= 0 && userx->played >= user->played && userx->won == user->won) {
         pos = x;
         x--;
@@ -81,20 +114,32 @@ int _get_sub_pos(const User *user, int pos) {
 
     x = pos;
     userx = (User *) items[x];
+    //check the list going down, until it no longer fits.
     while (x < control->users->count && userx->played < user->played && userx->won == user->won || userx == user) {
         pos = x;
         x++;
         userx = (User *) items[x];
     }
 
+    //return the newly fitted position
     return pos;
 }
 
+/**
+ * Updates the specified user's position. Should be run whenever the score changes.
+ *
+ * @param user the user to update
+ */
 void _update_user(User *user) {
     int pos = _get_score_pos(0, control->users->count - 1, user);
     List_move(control->users, user, pos);
 }
 
+/**
+ * Frees the memory for the leaderboard.
+ *
+ * @param l the leaderboard to free the memory of.
+ */
 void Free_leaderboard(Leaderboard *l) {
     if (l != NULL) {
         free(l->names);
