30c009353cb24b383001c79fe4dd5160047e39a3
Kevin Upton <k_upton@live.com>
2015-10-18 12:01:31 +1000

    recompiled server

--- Server/main.c
+++ Server/main.c
@@ -3,8 +3,6 @@
 //
 
 #include <stdio.h>
-#include <sys/unistd.h>
-#include <stdlib.h>
 #include "lib/control.h"
 
 int main(int argc, char *argv[]) {
@@ -12,54 +10,6 @@ int main(int argc, char *argv[]) {
         puts("Server requires a Port number");
     } else {
         control = newControl(argv);
-
-        User *u = User_login("Maolin");
-        User *j = User_login("Jason");
-        User *a = User_login("Anna");
-        User *p = User_login("Peter");
-
-        int i;
-        Leaderboard *l;
-
-        int win = 1; int lose = 0;
-        Leaderboard_result(a, &lose);
-        Leaderboard_result(a, &lose);
-        Leaderboard_result(u, &win);
-        Leaderboard_result(a, &win);
-        Leaderboard_result(u, &lose);
-        sleep(2);
-        l = newLeaderboard(0, 10);
-        for (i = l->count - 1; i >= 0; i--) {
-            printf("%d: %s %d %d\n", i, l->names[i], l->played[i], l->wins[i]);
-        }
-        sleep(4);
-        puts("\n");
-        Leaderboard_result(j, &win);
-        Leaderboard_result(j, &win);
-        Leaderboard_result(j, &win);
-        sleep(1);
-
-        l = newLeaderboard(0, 10);
-        for (i = l->count - 1; i >= 0; i--) {
-            printf("%d: %s %d %d\n", i, l->names[i], l->played[i], l->wins[i]);
-        }
-        puts("\n");
-        Leaderboard_result(p, &win);
-        Leaderboard_result(p, &win);
-        Leaderboard_result(p, &win);
-        Leaderboard_result(p, &lose);
-        Leaderboard_result(p, &lose);
-        Leaderboard_result(p, &lose);
-        Leaderboard_result(p, &win);
-
-        sleep(1);
-
-        l = newLeaderboard(0, 10);
-        for (i = l->count - 1; i >= 0; i--) {
-            printf("%d: %s %d %d\n", i, l->names[i], l->played[i], l->wins[i]);
-        }
-
-        exit(0);
 
         if (control->conn) {
             Controller_run();
